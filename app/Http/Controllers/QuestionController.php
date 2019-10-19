ff05069b8bcb77222ef5eb412256f81c3634e325
cafe <cafe153220@gmail.com>
2019-10-19 16:56:56 +0800

    CRUD for Question along with Question Resource Transformation

---
+++ app/Http/Controllers/QuestionController.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Model\Question;
+use App\Http\Resources\QuestionResource;
+use Illuminate\Http\Request;
+
+class QuestionController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return QuestionResource::collection(Question::latest()->get());
+    }
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        // auth()->user()->question->create($request->all());
+        Question::create($request->all());
+        return response('Created',201);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Model\Question  $question
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Question $question)
+    {
+        return new QuestionResource($question);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Model\Question  $question
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Question $question)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Model\Question  $question
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Question $question)
+    {
+        $question->delete();
+        return response(NULL, 204);
+    }
+}
---
+++ app/Model/Question.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Model;
+use App\User;
+// use App\Model\Reply;
+// use App\Model\Category;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Question extends Model
+{
+    public function getRouteKeyName(){
+        return 'slug';
+    }
+
+    // protected $fillable = ['title','slug','body','category_id','user_id'];
+    protected $guarded = [];
+    public function user(){
+        return $this->belongsTo(User::class);
+    }
+    public function replies(){
+        return $this->hasMany(Reply::class);
+    }
+    public function category(){
+        return $this->belongs(Category::class);
+    }
+
+    public function getPathAttribute(){
+        return asset("api/question/$this->slug");
+    }
+}
